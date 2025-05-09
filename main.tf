ce2c3b7fba1c96b905d6ccefdc32573e474ee2d1
Samir Sapkota <samirsapkota29@gmail.com>
2025-05-09 10:00:08 -0400

    10th commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "my-test-bucet710413vcfd3432"
+  bucket = "my-test-bucet710413vcvdfdfd3432"
 
   tags = {
     Name        = "My bucket"
