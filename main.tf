99849bf0f82283eaf91c839edb40af019cb51428
Samir Sapkota <samirsapkota29@gmail.com>
2025-05-09 00:02:53 -0400

    4th commit

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 
 }
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-samir710413"
+  bucket = "my-tf-test-bucket-samir710413-test"
 
   tags = {
     Name        = "My bucket"
